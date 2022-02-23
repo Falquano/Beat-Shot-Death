adbee309f3cbbb4dd45a337d4437490763706485
Victor Fanton <jiltofar@gmail.com>
2022-02-23 11:48:52 +0100

    Ajout d'animation de marche du joueur

---
+++ Assets/Player/PlayerAnimator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimator : MonoBehaviour
+{
+    private PlayerMove player;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GetComponentInParent<PlayerMove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", player.CurrentSpeed);
+    }
+}
