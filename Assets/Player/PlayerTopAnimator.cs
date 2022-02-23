1e9a5754cf9ea1bb90df244224ad5af1d5f07a02
Victor Fanton <jiltofar@gmail.com>
2022-02-23 16:21:08 +0100

    Intégration des animations de tir et de marche avant

---
+++ Assets/Player/PlayerTopAnimator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerTopAnimator : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private Vector2 blockedCheckOffset;
+    [SerializeField] private float blockedCheckRadius;
+    [SerializeField] private LayerMask blockedCheckMask;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // On est bloqué si il y a un collider dans la zone juste en face du joueur
+        bool blocked = Physics2D.OverlapCircle(transform.position + (Vector3)blockedCheckOffset, blockedCheckRadius, blockedCheckMask);
+        animator.SetBool("Blocked", blocked);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position + (Vector3) blockedCheckOffset, blockedCheckRadius);
+    }
+
+    public void OnShot(ShotInfo shotInfo)
+    {
+        if (shotInfo.Quality == ShotQuality.Okay)
+            animator.SetTrigger("OkayShot");
+        else if (shotInfo.Quality == ShotQuality.Perfect)
+            animator.SetTrigger("PerfectShot");
+    }
+}
