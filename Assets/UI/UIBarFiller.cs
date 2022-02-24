760c5fa6307bf64274d917f20313548163471c9f
Victor Fanton <jiltofar@gmail.com>
2022-02-24 15:59:36 +0100

    Ajout de smoothing sur la barre de combo

--- Assets/UI/UIBarFiller.cs
+++ Assets/UI/UIBarFiller.cs
@@ -11,6 +11,12 @@ public class UIBarFiller : MonoBehaviour
     private float progress;
     public float Progress { get => progress; set => SetProgress(value); }
 
+    private float visualProgress;
+    [SerializeField] private float interpolationTime;
+    [SerializeField] private AnimationCurve interpolationCurve;
+    private float interpolationOrigin;
+    private float timer;
+
     private void Start()
     {
         rect = GetComponent<RectTransform>();
@@ -18,12 +24,32 @@ public class UIBarFiller : MonoBehaviour
 
     private void SetProgress(float value)
     {
+        interpolationOrigin = progress;
         progress = value;
         UpdateBar();
+        timer = 0f;
+        enabled = true;
     }
 
     private void UpdateBar()
     {
-        rect.sizeDelta = new Vector2(Progress * maxWidth, rect.sizeDelta.y);
+        rect.sizeDelta = new Vector2(visualProgress * maxWidth, rect.sizeDelta.y);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        float timeProgress = interpolationCurve.Evaluate(timer / interpolationTime);
+
+        if (timeProgress >= 1f)
+        {
+            visualProgress = progress;
+            UpdateBar();
+            enabled = false;
+            return;
+        }
+
+        visualProgress = Mathf.Lerp(interpolationOrigin, Progress, timeProgress);
+        UpdateBar();
     }
 }
