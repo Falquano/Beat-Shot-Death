b0750774c877b1705e206e660e8f557e2fdcfc64
Camille Garneret <garneret.camillepro@gmail.com>
2022-05-01 13:45:09 +0100

    Test de l'anim seule des traits rouge de tempo

--- Assets/Player/TempoNew.cs
+++ Assets/Player/TempoNew.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class TempoNew : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Cette fonction va être appelé quand un tempo commence afin de vérif le combo et en fonction du combo de changer la vitesse de l'anim du cursor et activé la bool
+    private int Combo;
+    [SerializeField] private Animator tempoAnim;
 
-    // Update is called once per frame
-    void Update()
+    public void OnCursorAnim(int combo)
     {
-        
+        Combo = combo;
+
+        if (combo >= 0 && combo <= 20)
+        {
+            //Animdépart.lenght on change sa duration
+            //float length1 = tempoA
+            //Animphase1.lenght on change sa duration
+            //Animphase2.lenght on change sa duration
+
+            tempoAnim.SetTrigger("Begin");
+
+        }
+        else if (combo > 20 && combo <= 40)
+        {
+            //Animdépart.lenght on change sa duration
+            //Animphase1.lenght on change sa duration
+            //Animphase2.lenght on change sa duration
+
+            tempoAnim.SetTrigger("Begin");
+        }
+        else if (combo > 40 && combo <= 70)
+        {
+            //Animdépart.lenght on change sa duration
+            //Animphase1.lenght on change sa duration
+            //Animphase2.lenght on change sa duration
+
+            tempoAnim.SetTrigger("Begin");
+        }
+        else if (combo > 70 && combo <= 90)
+        {
+            //Animdépart.lenght on change sa duration
+            //Animphase1.lenght on change sa duration
+            //Animphase2.lenght on change sa duration
+
+            tempoAnim.SetTrigger("Begin");
+        }
+        else
+        {
+            //Animdépart.lenght on change sa duration
+            //Animphase1.lenght on change sa duration
+            //Animphase2.lenght on change sa duration
+
+            tempoAnim.SetTrigger("Begin");
+        }
     }
 }
