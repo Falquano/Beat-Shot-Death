e08157fde21fe52daf976ad7e6ec1fb67fa685b3
Camille Garneret <garneret.camillepro@gmail.com>
2022-05-07 16:15:37 +0100

    Ajout du code de vfx onde, denis à juste à les refaire bien, les placer et les lancé

--- Assets/VFXOndeScript.cs
+++ Assets/VFXOndeScript.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 
 public class VFXOndeScript : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject Onde1;
+    [SerializeField] private GameObject Onde2;
+    [SerializeField] private GameObject Onde3;
+    [SerializeField] private GameObject Onde4;
+
+    //Oubli pas de placer tes vfx sur l'objet ombo et de bien les placer au centre
+    //Ensuite tu les déplacer dans l'inspecteur de ce script dans le bon ordre
+    //Puis tu met juste la ligne de code qui permet de lire un vfx
     public void OnVFXOndeActive(float combo)
     {
         if(combo >= 0 && combo < 1)
         {
-            print("caca");
+            print("combo à 0");
         }
         else if(combo >= 1 && combo < 10)
         {
-
+            //Play l'onde1
         }
         else if (combo >= 10 && combo < 30)
         {
-
+            //Play onde 2
         }
         else if (combo >= 30 && combo < 60)
         {
-
+            //Play onde 3
         }
         else if (combo >= 60 && combo < 100)
         {
-
+            //Play Onde 4
         }
     }
 }
