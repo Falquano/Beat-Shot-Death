e92439b25e1043e3a656ab6c683d1a14341b3330
FrancoisJourdainArxenn <jourdain.f@live.fr>
2022-05-11 17:13:48 +0200

    Sword marche pas

--- Assets/Ennemies/EnnemySoundEffects.cs
+++ Assets/Ennemies/EnnemySoundEffects.cs
@@ -11,19 +11,31 @@ public class EnnemySoundEffects : MonoBehaviour
     [SerializeField] private StudioEventEmitter HurtSoundEmitter;
     [SerializeField] private StudioEventEmitter DieSoundEmitter;
 
+
     public void OnShoot()
     {
         EmitShotSound();
+        Debug.Log("jappel l'attaque");
+
+    }
+
+    public void OnHit()
+    {
+        EmitShotSound();
+        Debug.Log("jappel l'attaque");
+
     }
 
     private void EmitShotSound()
     {
         GunshotSoundEmitter.Play();
+        Debug.Log("j'attaque");
     }
 
     public void OnFootstep()
     {
         FootstepSoundEmitter.Play();
+        //Debug.Log("JeMarche");
     }
 
     public void HurtSoundEffect(int damage, int newLife)
