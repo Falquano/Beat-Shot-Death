1e8984c19416737b853be3af9db0140a7b93ec65
Camille Garneret <garneret.camillepro@gmail.com>
2022-05-19 11:07:43 +0100

    Les ondes se placent nickel et sont visibles - J'ai fais un raycast de la caméra à la souris qui collide vec un plan - Je donne la position du hitpoint aux ondes, et voilà

--- Assets/OndeScriptVFX.cs
+++ Assets/OndeScriptVFX.cs
@@ -10,9 +10,12 @@ public class OndeScriptVFX : MonoBehaviour
 {
     [SerializeField] private ShootPlayer ScriptShootPlayer;
     private int ComboPlayer;
+    private Vector3 MousePositionOnPlanOnde;
 
     [SerializeField] private List<VisualEffect> VFXOndeList = new List<VisualEffect>();
-    
+    [SerializeField] private LayerMask MaskPlanOnde;
+
+
 
     private void Start()
     {
@@ -46,9 +49,23 @@ public class OndeScriptVFX : MonoBehaviour
     private void Update()
     {
        
-        Vector3 OndePosition = new Vector3(ScriptShootPlayer.MouseWorldPosition.x, ScriptShootPlayer.MouseWorldPosition.y, ScriptShootPlayer.MouseWorldPosition.z);
+        /*Vector3 OndePosition = new Vector3(ScriptShootPlayer.MouseWorldPosition.x, ScriptShootPlayer.MouseWorldPosition.y, ScriptShootPlayer.MouseWorldPosition.z);
         transform.position = OndePosition;
-        transform.rotation = new Quaternion(71.74f, 0f, -6.7f, 0f);
+        transform.rotation = new Quaternion(71.74f, 0f, -6.7f, 0f);*/
+
+
+        Ray pointerRay = Camera.main.ScreenPointToRay(ScriptShootPlayer.MouseScreenPosition);
+        if (Physics.Raycast(pointerRay, out RaycastHit hitInfo, float.MaxValue, MaskPlanOnde))
+        {
+            transform.position = hitInfo.point;
+            
+            
+            
+            /*Vector3 direction = MousePositionOnPlanOnde - transform.position;
+            direction.y = 0;
+            transform.forward = direction.normalized;
+            Debug.DrawRay(transform.position, transform.right * 4, Color.white);*/
+        }
     }
     
 
