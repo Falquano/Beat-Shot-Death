104e43cae6bac7477b72932624bd36f4a8135cee
Camille Garneret <garneret.camillepro@gmail.com>
2022-06-02 15:18:09 +0100

    push

--- Assets/ManagerScripts/LevelChanger.cs
+++ Assets/ManagerScripts/LevelChanger.cs
@@ -6,12 +6,7 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class LevelChanger : MonoBehaviour
 {
-    [SerializeField] private string levelToLoad;
-
-    public void GotoLevel(CallbackContext context)
-    {
-            SceneManager.LoadScene("Hall_2");
-    }
+   
 
     public void GotoLevel()
     {
